0e868b8ef84321094cdbeee6e88477d4f003dadd
brownrice55 <113149599+brownrice55@users.noreply.github.com>
2026-01-20 15:24:48 +0900

    feat: add functions to close the navigation bar

--- vanilla/src/main.ts
+++ vanilla/src/main.ts
@@ -1,6 +1,11 @@
 import './style.scss';
+import 'bootstrap';
 import { getDataFromLocalStorage } from './modules/dataManagement';
-import { setupInitialDisplay, switchPage } from './modules/display';
+import {
+  setupInitialDisplay,
+  switchPage,
+  closeGlobalMenu,
+} from './modules/display';
 import type { Inputs } from './types/inputs.type';
 
 document.body.classList.add('loaded');
@@ -8,15 +13,19 @@ document.body.classList.add('loaded');
 const quizCategory = getDataFromLocalStorage('quizCategory');
 const quizData = getDataFromLocalStorage('quizData');
 
-const globalNavLiElms =
-  document.querySelectorAll<HTMLLIElement>('.js-globalNav li');
+const globalNavElm = document.querySelector<HTMLElement>('.js-globalNav');
+const globalNavLiElms = globalNavElm?.querySelectorAll<HTMLLIElement>('li');
 
 setupInitialDisplay(quizCategory as string[], quizData as Map<number, Inputs>);
 
-globalNavLiElms.forEach((elm) => {
+globalNavLiElms?.forEach((elm) => {
   elm.addEventListener('click', function (e: MouseEvent) {
     const target = e.currentTarget as HTMLElement;
     const listIndex = Number(target.dataset.index);
     switchPage(listIndex);
   });
 });
+
+if (globalNavElm) {
+  closeGlobalMenu(globalNavElm);
+}
--- vanilla/src/modules/display.ts
+++ vanilla/src/modules/display.ts
@@ -1,3 +1,5 @@
+import { Collapse } from 'bootstrap';
+
 const funcForDisplay = (aIndex: number) => {
   const sectionElms = document.querySelectorAll<HTMLElement>('.js-section')!;
   sectionElms.forEach((elm) => {
@@ -17,4 +19,27 @@ export function setupInitialDisplay(
 
 export function switchPage(aIndex: number) {
   funcForDisplay(aIndex);
+}
+
+export function closeGlobalMenu(aGlobalNavElm: HTMLElement) {
+  const bsCollapse = new Collapse(aGlobalNavElm, { toggle: false });
+
+  const mediaQueryList = window.matchMedia('(max-width: 992px)');
+  const hideMenus = (matches: boolean) => {
+    if (matches) {
+      document.addEventListener('click', function (e: MouseEvent) {
+        const target = e.target;
+        if (target instanceof Node) {
+          bsCollapse.hide();
+        }
+      });
+    }
+  };
+
+  const listener = (e: MediaQueryListEvent) => {
+    hideMenus(e.matches);
+  };
+
+  mediaQueryList.addEventListener('change', listener);
+  hideMenus(mediaQueryList.matches);
 }
